05cae27317537597e6bc5561a279086de71c2583
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2023-02-09 14:08:48 -0300

    feat: fetch in pokemon api using name param

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <input v-model="inputedSearch" />
+  <button @click="fetchPokemonApi">Procurar pokemon</button>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import useApiPokemon from "../composables/useApiPokemon";
+const { getPokemonByName } = useApiPokemon();
+
+const pokemon = ref({});
+
+const inputedSearch = ref("");
+
+const fetchPokemonApi = async () => {
+  try {
+    pokemon.value = await getPokemonByName(inputedSearch.value);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/HomePokedex.vue
@@ -0,0 +1,9 @@
+<script setup>
+import PokemonCard from "../components/PokemonCard.vue";
+</script>
+
+<template>
+  <main>
+    <PokemonCard />
+  </main>
+</template>
