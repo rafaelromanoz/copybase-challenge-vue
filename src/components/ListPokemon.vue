42c2a6f4cb787bbeb285264b843b32bd728e0c3e
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2023-02-10 08:22:03 -0300

    feat: renderize pokemon and images

--- src/components/ListPokemon.vue
+++ src/components/ListPokemon.vue
@@ -1,7 +1,20 @@
 <template>
   <div v-if="existsPoke">
     <p>{{ getPoke.name }}</p>
-    <img :src="getPoke.sprites.front_default" alt="pokemon" />
+    <img :src="getPoke.sprites.front_default" alt="foundPokemon" />
+    <p>Evoluções</p>
+    <p>{{ pokeEvolutions.baby.name }}</p>
+    <img
+      :src="pokeEvolutions.baby.sprites.front_default"
+      alt="babyEvolutionPokemon"
+    />
+    <p>{{ pokeEvolutions.first.name }}</p>
+    <img
+      :src="pokeEvolutions.first.sprites.front_default"
+      alt="firstEvolutionPokemon"
+    />
+    <p>{{ pokeEvolutions.second.name }}</p>
+    <img :src="pokeEvolutions.second.sprites.front_default" alt="secondEvolutionPokemon" />
   </div>
   <div v-else>
     <p>Procure um pokemon!</p>
@@ -14,6 +27,10 @@ const store = pokemonStore();
 const getPoke = computed(() => {
   return store.getPokeInfoSpecie;
 });
+const pokeEvolutions = computed(() => {
+  return store.getPokeInfoEvolutionChains;
+});
+
 const existsPoke = computed(() => {
   return !(!getPoke.value || Object.keys(getPoke.value).length === 0);
 });
--- src/stores/pokemon.js
+++ src/stores/pokemon.js
@@ -1,12 +1,16 @@
 import { defineStore } from "pinia";
 import api from "../config/axios";
 
-const formatUrl = (url) => url.split('/v2/')[1]
+const formatUrl = (url) => url.split("/v2/")[1];
 
 export const pokemonStore = defineStore("pokemon", {
   state: () => ({
     foundPokemonBySpecie: {},
-    foundPokemonEvolutionChains: {},
+    foundPokemonEvolutionChains: {
+      baby: {},
+      first: {},
+      second: {},
+    },
   }),
   getters: {
     getPokeInfoSpecie(state) {
@@ -17,12 +21,33 @@ export const pokemonStore = defineStore("pokemon", {
     },
   },
   actions: {
+    async getInfoPokeEvolutions(pokemonEvolutionChains) {
+      const promiseBabyEvolutionPoke = api.get(
+        `pokemon/${pokemonEvolutionChains.chain.species.name}`
+      );
+      const promiseFirstEvolutionPoke = api.get(
+        `pokemon/${pokemonEvolutionChains.chain.evolves_to[0]?.evolves_to[0]?.species.name}`
+      );
+      const promiseSecondEvolutionPoke = api.get(
+        `pokemon/${pokemonEvolutionChains.chain.evolves_to[0]?.species.name}`
+      );
+      const [baby, first, second] = await Promise.all([
+        promiseBabyEvolutionPoke,
+        promiseFirstEvolutionPoke,
+        promiseSecondEvolutionPoke,
+      ]);
+      this.foundPokemonEvolutionChains.baby = baby.data;
+      this.foundPokemonEvolutionChains.first = first.data;
+      this.foundPokemonEvolutionChains.second = second.data;
+    },
     async getPokemonBySpecieAndEvolutionChain(pokemonName) {
       const { data } = await api.get(`pokemon-species/${pokemonName}/`);
-      const pokemonData = await api.get(formatUrl(data.varieties[0]?.pokemon?.url));
+      const pokemonData = await api.get(
+        formatUrl(data.varieties[0]?.pokemon?.url)
+      );
       this.foundPokemonBySpecie = pokemonData.data;
       const response = await api.get(formatUrl(data.evolution_chain.url));
-      this.foundPokemonEvolutionChains = response.data;
+      await this.getInfoPokeEvolutions(response.data);
     },
   },
 });
